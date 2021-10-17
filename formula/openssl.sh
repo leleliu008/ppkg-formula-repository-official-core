aae8d717ed51339f0c8183ba6502e8cde3958d77
leleliu008 <leleliu008@gmail.com>
2021-10-17 12:54:59 +0800

    optimized

--- formula/openssl.sh
+++ formula/openssl.sh
@@ -1,7 +1,9 @@
 package set summary "Cryptography and SSL/TLS Toolkit"
 package set webpage "https://openssl.org"
-package set src.url "https://dl.bintray.com/homebrew/mirror/openssl-1.1.1g.tar.gz"
+package set src.git "https://github.com/openssl/openssl.git"
+package set src.url "https://www.openssl.org/source/openssl-1.1.1g.tar.gz"
 package set src.sum "ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46"
+package set license "OpenSSL"
 package set bsystem "make"
 package set dep.cmd "perl"
 package set binsrcd 'YES'
