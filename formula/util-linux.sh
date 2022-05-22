ecd56b3e017b6f19258a42417a8ce00d9c0fa2fc
leleliu008 <leleliu008@gmail.com>
2022-05-22 20:28:37 +0800

    optimized

--- formula/util-linux.sh
+++ formula/util-linux.sh
@@ -8,18 +8,18 @@ pkg_set bsystem "configure"
 
 if [ "$NATIVE_OS_KIND" = darwin ] ; then
     # Fix ./include/statfs_magic.h:4:10: fatal error: 'sys/statfs.h' file not found
-    pkg_set fix.url "https://github.com/util-linux/util-linux/commit/478b9d477ecdd8f4e3a7b488524e1d4c6a113525.patch"
+    pkg_set fix.url "https://github.com/util-linux/util-linux/commit/478b9d477ecdd8f4e3a7b488524e1d4c6a113525.patch?full_index=1"
     pkg_set fix.sha "576c26c3d15642f1c44548d0120b192b855cceeebf8ad97fb5e300350e88a3f7"
 
     # Fix lib/procfs.c:9:10: fatal error: 'sys/vfs.h' file not found
-    pkg_set res.url "https://github.com/util-linux/util-linux/commit/3671d4a878fb58aa953810ecf9af41809317294f.patch"
+    pkg_set res.url "https://github.com/util-linux/util-linux/commit/3671d4a878fb58aa953810ecf9af41809317294f.patch?full_index=1"
     pkg_set res.sha "d38c9ae06c387da151492dd5862c58551559dd6d2b1877c74cc1e11754221fe4"
 fi
 
 prepare() {
     if [ "$NATIVE_OS_KIND" = darwin ] ; then
-        patch -p1 < ../fix/478b9d477ecdd8f4e3a7b488524e1d4c6a113525.patch
-        patch -p1 < ../res/3671d4a878fb58aa953810ecf9af41809317294f.patch
+        patch -p1 < ../fix/*
+        patch -p1 < ../res/*
     fi
 }
 
