5d290b0cef39712741e7c31682cd3325b1bbe0d0
leleliu008 <leleliu008@gmail.com>
2022-05-22 19:03:54 +0800

    optimized

--- formula/jq.sh
+++ formula/jq.sh
@@ -8,5 +8,11 @@ pkg_set depends "oniguruma bison"
 pkg_set bsystem "configure"
 
 build() {
+    # https://www.gnu.org/software/gnulib/manual/html_node/lgamma_005fr.html
+    if [ "$NATIVE_OS_KIND" = darwin ] ; then
+        export ac_cv_lib_m_lgamma_r=no
+        export MACOSX_DEPLOYMENT_TARGET=10.8
+    fi
+
     configure --with-oniguruma="$oniguruma_INSTALL_DIR"
 }
