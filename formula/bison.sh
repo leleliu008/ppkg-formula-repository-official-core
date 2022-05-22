619c8e5d3fb32582ee1cff5fcfc9b280566a67dc
leleliu008 <leleliu008@gmail.com>
2022-05-22 16:32:25 +0800

    optimized

--- formula/bison.sh
+++ formula/bison.sh
@@ -2,7 +2,9 @@ pkg_set summary "Yacc-compatible Parser generator"
 pkg_set webpage "https://www.gnu.org/software/bison"
 pkg_set src.url "https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.xz"
 pkg_set src.sha "9bba0214ccf7f1079c5d59210045227bcf619519840ebfa80cd3849cff5a5bf2"
+pkg_set git.url "https://git.savannah.gnu.org/git/bison.git"
 pkg_set license "GPL-3.0-or-later"
+pkg_set depends "m4"
 pkg_set bsystem "configure"
 
 build() {
--- formula/jq.sh
+++ formula/jq.sh
@@ -4,11 +4,9 @@ pkg_set git.url "https://github.com/stedolan/jq.git"
 pkg_set src.url "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz"
 pkg_set src.sha "5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72"
 pkg_set license "MIT"
-pkg_set depends "oniguruma"
+pkg_set depends "oniguruma bison"
 pkg_set bsystem "configure"
 
 build() {
-    configure \
-        --disable-maintainer-mode \
-        --with-oniguruma="$oniguruma_INSTALL_DIR"
+    configure --with-oniguruma="$oniguruma_INSTALL_DIR"
 }
