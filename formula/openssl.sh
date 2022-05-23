fdaa20bf0ddf9b064e25c9be839d0cb1c5997287
leleliu008 <leleliu008@gmail.com>
2022-05-23 19:28:51 +0800

    optimized

--- formula/openssl.sh
+++ formula/openssl.sh
@@ -1,10 +1,10 @@
 pkg_set summary "Cryptography and SSL/TLS Toolkit"
 pkg_set webpage "https://openssl.org"
 pkg_set git.url "https://github.com/openssl/openssl.git"
-pkg_set src.url "https://www.openssl.org/source/openssl-1.1.1n.tar.gz"
-pkg_set src.sha "40dceb51a4f6a5275bde0e6bf20ef4b91bfc32ed57c0552e2e8e15463372b17a"
+pkg_set src.url "https://www.openssl.org/source/openssl-1.1.1o.tar.gz"
+pkg_set src.sha "9384a2b0570dd80358841464677115df785edb941c71211f75076d72fe6b438f"
 pkg_set license "OpenSSL"
-#pkg_set depends "perl"
+pkg_set depends "perl"
 pkg_set bsystem "gmake"
 pkg_set binbstd "yes"
 pkg_set ldflags "-Wl,-z,muldefs"
@@ -13,7 +13,7 @@ pkg_set ldflags "-Wl,-z,muldefs"
 #https://github.com/openssl/openssl/issues/7582
 build() {
     run ./config \
-        shared \
+        no-shared \
         no-ssl2 \
         no-ssl3 \
         no-comp \
