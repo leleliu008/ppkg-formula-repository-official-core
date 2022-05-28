e0d250f9e329b9288158a414096c78698d03aa07
leleliu008 <leleliu008@gmail.com>
2022-05-28 14:29:50 +0800

    optimized

--- formula/libzip.sh
+++ formula/libzip.sh
@@ -1,18 +1,24 @@
 pkg_set summary "C library for reading, creating, and modifying zip archives"
 pkg_set webpage "https://libzip.org"
-pkg_set src.url "https://libzip.org/download/libzip-1.7.0.tar.xz"
-pkg_set src.sha "d26b2952426d2518f3db5cdeda4fe3cd668fc5bb38a598781e4d1d3f7f8ca7be"
+pkg_set src.url "https://libzip.org/download/libzip-1.8.0.tar.xz"
+pkg_set src.sha "f0763bda24ba947e80430be787c4b068d8b6aa6027a26a19923f0acfa3dac97e"
+pkg_set git.url "https://github.com/nih-at/libzip.git"
+pkg_set depends "zstd bzip2"
 pkg_set bsystem "cmake"
 
 build() {
     cmakew \
-    -DENABLE_GNUTLS=OFF \
-    -DENABLE_OPENSSL=OFF \
-    -DENABLE_MBEDTLS=OFF \
-    -DENABLE_COMMONCRYPTO=OFF \
-    -DENABLE_WINDOWS_CRYPTO=OFF \
-    -DBUILD_DOC=OFF \
-    -DBUILD_TOOLS=OFF \
-    -DBUILD_REGRESS=OFF \
-    -DBUILD_EXAMPLES=OFF
+        -DENABLE_ZSTD=ON \
+        -DENABLE_LZMA=ON \
+        -DENABLE_BZIP2=ON \
+        -DENABLE_GNUTLS=OFF \
+        -DENABLE_OPENSSL=OFF \
+        -DENABLE_MBEDTLS=OFF \
+        -DENABLE_COMMONCRYPTO=OFF \
+        -DENABLE_WINDOWS_CRYPTO=OFF \
+        -DBUILD_DOC=OFF \
+        -DBUILD_TOOLS=OFF \
+        -DBUILD_REGRESS=OFF \
+        -DBUILD_EXAMPLES=OFF
+    sed_in_place 's|-lZstd|-lzstd|' "$PACKAGE_INSTALL_DIR/lib/pkgconfig/libzip.pc"
 }
