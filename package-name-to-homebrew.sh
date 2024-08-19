9fecad87cb3287f065fd42a4a168f1d54dd391d9
leleliu008 <leleliu008@gmail.com>
2024-08-19 20:34:17 +0800

    optimized

--- package-name-to-homebrew.sh
+++ package-name-to-homebrew.sh
@@ -142,6 +142,8 @@ case $1 in
     libeigen) printf '%s\n' 'eigen'     ;;
     libcares) printf '%s\n' 'c-ares'    ;;
     libfolly) printf '%s\n' 'folly'     ;;
+    libbotan) printf '%s\n' 'botan'     ;;
+    libproj)  printf '%s\n' 'proj'      ;;
     libsrtp)  printf '%s\n' 'srtp'      ;;
     libflac)  printf '%s\n' 'flac'      ;;
     libx264)  printf '%s\n' 'x264'      ;;
