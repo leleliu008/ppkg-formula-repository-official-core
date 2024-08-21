e24fdb7d78ec84aec91aa451c30846eb1f29e8f8
leleliu008 <leleliu008@gmail.com>
2024-08-21 21:33:59 +0800

    optimized

--- package-name-to-homebrew.sh
+++ package-name-to-homebrew.sh
@@ -204,6 +204,7 @@ case $1 in
     libtbb)   printf '%s\n' 'tbb'       ;;
     libsrt)   printf '%s\n' 'srt'       ;;
     libwv2)   printf '%s\n' 'wv2'       ;;
+    libdb)    printf '%s\n' 'berkeley-db' ;;
     libz3)    printf '%s\n' 'z3'        ;;
     libz)     printf '%s\n' 'zlib'      ;;
 
